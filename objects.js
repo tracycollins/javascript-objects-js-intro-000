961c0646b9c28b48f71de334040a08edebd0c1a1
Tracy Collins <tc@threeceemedia.com>
2020-02-27 05:40:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,3 +9,12 @@ function updatePlaylist(playlist, artistName, songTitle) {
   newPlaylist[artistName] = songTitle
   return newPlaylist;
 }
+
+function removeFromPlaylist(playlist, artistName, songTitle) {
+  console.log(artistName)
+  console.log(songTitle)
+  var newPlaylist = Object.assign({}, playlist);
+  console.log(newPlaylist)
+  newPlaylist{[artistName]: songTitle}
+  return newPlaylist;
+}
