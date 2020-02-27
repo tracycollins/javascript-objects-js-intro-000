15c62dcf0c853a9e42f34306233ea4b1611b4636
Tracy Collins <tc@threeceemedia.com>
2020-02-27 05:32:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = new Object();
 playlist["coltrane"] = "naima"
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var newPlaylist = Object.assign({}, playlist)
-  newPlaylist[artistName] = songTitle
-  return newPlaylist
+  var newPlaylist = Object.assign({}, playlist);
+  newPlaylist[artistName] = songTitle;
+  return newPlaylist;
 }
