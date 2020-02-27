bd65c677fa0c53a3d8b7672bd6e2b6c5bc44fb54
Tracy Collins <tc@threeceemedia.com>
2020-02-27 05:38:16 +0000

    Automatically backed up by Learn

--- test/objects-test.js
+++ test/objects-test.js
@@ -28,7 +28,7 @@ describe('objects', () => {
     })
 
     it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
       expect(playlist).
         to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
